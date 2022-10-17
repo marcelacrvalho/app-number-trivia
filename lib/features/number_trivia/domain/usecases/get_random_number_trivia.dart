f0f3f91e8c84b66cef38b549208d420b29c9a3c5
marcelacrvalho <marcelacrvalho@icloud.com>
2022-10-17 14:03:08 -0300

    Refatora GetRandomNumberTrivia e adiciona NoParams class

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -1,18 +1,21 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 
 import '../../../../core/core.dart';
 import '../domain.dart';
 
-class GetRandomNumberTrivia implements Usecase<NumberTrivia, Params> {
+class GetRandomNumberTrivia implements Usecase<NumberTrivia, NoParams> {
   final NumberTriviaRepository repository;
 
   GetRandomNumberTrivia(this.repository);
 
   @override
-  Future<Either<Failure, NumberTrivia>> call(Params params) async {
+  Future<Either<Failure, NumberTrivia>> call(NoParams params) async {
       return await repository.getRandomNumberTrivia();
   }
+}
 
-
-
+class NoParams extends Equatable {
+  @override
+  List<Object?> get props => throw UnimplementedError();
 }

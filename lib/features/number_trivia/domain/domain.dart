bc4c0965ed8fbc3e4623a212069226b6014f7f48
marcelacrvalho <marcelacrvalho@icloud.com>
2022-10-17 13:12:02 -0300

    Adiciona GetConcreteNumberTrivia ao domain

--- lib/features/number_trivia/domain/domain.dart
+++ lib/features/number_trivia/domain/domain.dart
@@ -1,2 +1,3 @@
 export 'entities/number_trivia.dart';
 export 'repositories/number_trivia_repository.dart';
+export 'usecases/get_concrete_number_trivia.dart';
---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/core.dart';
+import '../domain.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  GetConcreteNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> execute({required int number}) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
+}

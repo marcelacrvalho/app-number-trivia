1e65aef339fa9f16fc1a3a27715294be64b6b848
marcelacrvalho <marcelacrvalho@icloud.com>
2022-10-18 16:19:43 -0300

    Adiciona e implementa classe NumberTriviaLocalDataSourceImpl

--- lib/features/number_trivia/data/datasources/number_trivia_local_datasource.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_local_datasource.dart
@@ -1,6 +1,31 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import '../../../../core/core.dart';
 import '../data.dart';
 
 abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
   Future<void> cacheNumberTrivia(NumberTriviaModel numberTriviaModel);
+}
+
+const CACHED_NUMBER_TRIVIA = 'CACHED_NUMBER_TRIVIA';
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    final jsonString = sharedPreferences.getString(CACHED_NUMBER_TRIVIA);
+    return jsonString != null
+        ? Future.value(NumberTriviaModel.fromJson(json.decode(jsonString)))
+        : throw CacheException();
+  }
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache) async {
+    sharedPreferences.setString(
+        CACHED_NUMBER_TRIVIA, jsonEncode(triviaToCache.toJson()));
+  }
 }
